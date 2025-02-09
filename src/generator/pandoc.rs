6953eb335a51ff263b18181841bfdcc71b763186
Alex Abdugafarov <alexander.abdugafarov@nethermind.io>
2025-02-10 00:57:05 +0500

    Generator now skips Pandoc run if the desired output is markdown

--- src/generator/pandoc.rs
+++ src/generator/pandoc.rs
@@ -60,17 +60,21 @@ impl Generator for PandocGenrator {
 
     async fn finalize(&mut self) -> Result<(), TranslationError> {
         self.translated_md_file = None;
-        let temp_md_path = self.translated_md_path.clone();
+        let translated_md_path = self.translated_md_path.clone();
         let output_path = self.output_path.clone();
-        tokio::task::spawn_blocking(move || {
-            let mut pandoc = pandoc::new();
-            pandoc.add_input(&temp_md_path);
-            pandoc.set_output(OutputKind::File(output_path));
-            pandoc.execute()
-        })
-        .await
-        .map_err(|e| TranslationError::OtherError(e.into()))?
-        .map_err(|e| TranslationError::OtherError(e.into()))?;
+
+        // If output file itself is Markdown, no need to run pandoc
+        if translated_md_path != output_path {
+            tokio::task::spawn_blocking(move || {
+                let mut pandoc = pandoc::new();
+                pandoc.add_input(&translated_md_path);
+                pandoc.set_output(OutputKind::File(output_path));
+                pandoc.execute()
+            })
+            .await
+            .map_err(|e| TranslationError::OtherError(e.into()))?
+            .map_err(|e| TranslationError::OtherError(e.into()))?;
+        }
 
         Ok(())
     }
